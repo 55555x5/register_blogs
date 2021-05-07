508906c08dc02e743253ce75cd42609e984ee730
Lucas <83518257+lu700@users.noreply.github.com>
2021-05-07 11:45:12 -0300

    @7ort

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php $blogs=[
   ["lucas","https://raw.githubusercontent.com/lu700/blog/main/blog_config.php"],
-  ["7ort","https://github.com/7ort/7/raw/main/7.php"]
+  ["7ort","https://raw.githubusercontent.com/7ort/7/main/7.php"]
 ]; ?>
