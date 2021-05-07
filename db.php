c03f42aa30cb5ea59bfb4409b7dc21460b9e4312
Lucas <83518257+lu700@users.noreply.github.com>
2021-05-07 11:20:51 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,4 @@
 <?php $blogs=[
-  ["lucas","https://raw.githubusercontent.com/lu700/blog/main/blog_config.php"]
+  ["lucas","https://raw.githubusercontent.com/lu700/blog/main/blog_config.php"],
+  ["7ort","https://github.com/7ort/7/raw/main/7.php"]
 ]; ?>
