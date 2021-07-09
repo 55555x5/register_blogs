f081bb4f65ac5ddb0b8dce68260e8bdd4c3cb3c7
Lucas <83518257+lu700@users.noreply.github.com>
2021-07-08 22:44:25 -0300

    Now 7ort Blog now works

--- db.php
+++ db.php
@@ -3,5 +3,5 @@
   Don't forget to put commas between registries */
   ["lucas","https://raw.githubusercontent.com/lu700/blogs/main/blog_config.php"],
   ["7ort","https://raw.githubusercontent.com/7ort/7/main/7.php"],
-  ["frifoss","https://raw.githubusercontent.com/lu700/blogs/main/frifoss.php""]
+  ["frifoss","https://raw.githubusercontent.com/lu700/blogs/main/frifoss.php"]
 ]; ?>
