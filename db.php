6abb01f4d8bb18810079e8d52c978bfcfb102910
Lucas <83518257+lucotoh@users.noreply.github.com>
2021-07-20 20:21:57 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php $blogs=[
   /* Add this: ["(blog's username)","(url of your file)"]
   Don't forget to put commas between registries */
-  ["lucas","https://raw.githubusercontent.com/lu700/blogs/main/blog_config.php"],
+  ["lucas","https://raw.githubusercontent.com/lucotoh/blogs/main/blog_config.php"],
   ["7ort","https://raw.githubusercontent.com/7ort/7/main/7.php"],
-  ["frifoss","https://raw.githubusercontent.com/lu700/blogs/main/frifoss.php"]
+  ["frifoss","https://raw.githubusercontent.com/lucotoh/blogs/main/frifoss.php"]
 ]; ?>
